26ddabf80423fbe59e60d108c0633f98839e04e8
Julian Tran <juliantran003@gmail.com>
2023-06-27 22:37:03 +0800

    added how it works basic

--- pages/how.js
+++ pages/how.js
@@ -1,3 +1,15 @@
+// JSON
+import how from "../json/how.json";
+
 export default function How() {
-  return <section></section>;
+  return (
+    <section className="how">
+      <h2>How does it work ?</h2>
+      <ul>
+        {how.map((how) => {
+          return <li key={how}>{how}</li>;
+        })}
+      </ul>
+    </section>
+  );
 }
